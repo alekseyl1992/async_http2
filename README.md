6d0a6896d2085bca08e0f13a5f21757d702b93d7
alekseyl <alekseyl@list.ru>
2021-07-27 21:53:25 +0300

    use rust_tls for http2_prior_knowledge

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# asyncio compatible HTTP2 client for Python based on `reqwest` Rust crate and pyo3/pyo3-asyncio
+### asyncio-compatible HTTP2 client for Python based on `reqwest` Rust crate and pyo3/pyo3-asyncio
--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,11 @@ struct Client {
 #[pymethods]
 impl Client {
     #[new]
-    pub fn new(timeout: u64) -> PyResult<Self> {
+    pub fn new(timeout: u64, accept_invalid_certs: bool) -> PyResult<Self> {
         let client = match reqwest::Client::builder()
+            .use_rustls_tls()
+            .tls_built_in_root_certs(true)
+            .danger_accept_invalid_certs(accept_invalid_certs)
             .http2_prior_knowledge()
             .timeout(Duration::from_secs(timeout))
             .build() {
