4984fefa77ff67aa4460b22e01a3a84eae467689
alekseyl <alekseyl@list.ru>
2021-07-29 20:20:31 +0300

    add RetryError to handle NO_ERROR errors

--- src/lib.rs
+++ src/lib.rs
@@ -62,11 +62,26 @@ impl Client {
 }
 
 fn make_exception(desc: &str, e: Error) -> PyErr {
-    let root_cause_desc = match e.source() {
+    let source_desc = match e.source() {
         Some(s) => s.to_string(),
         None => "".to_string(),
     };
-    let msg = format!("[{}] {}: {}", root_cause_desc, desc, e.to_string());
+    let msg = format!("[{}] {}: {}", source_desc, desc, e.to_string());
+
+    // check if it is NO_ERROR(0) error
+    // see:
+    // https://github.com/seanmonstar/reqwest/issues/1276
+    // https://github.com/hyperium/hyper/issues/2500
+    let no_error = match find_source::<h2::Error>(&e) {
+        Some(h2_error) => match h2_error.reason() {
+            Some(reason) => u32::from(reason) == 0,
+            None => false,
+        },
+        None => false,
+    };
+    if no_error {
+        return PyErr::new::<RetryError, _>(msg);
+    }
 
     if e.is_timeout() {
         PyErr::new::<TimeoutError, _>(msg)
@@ -89,6 +104,19 @@ fn make_exception(desc: &str, e: Error) -> PyErr {
     }
 }
 
+fn find_source<E: StdError + 'static>(e: &Error) -> Option<&E> {
+    let mut cause = e.source();
+    while let Some(err) = cause {
+        if let Some(ref typed) = err.downcast_ref() {
+            return Some(typed);
+        }
+        cause = err.source();
+    }
+
+    // else
+    None
+}
+
 create_exception!(async_http2, TimeoutError, pyo3::exceptions::PyTimeoutError);
 create_exception!(async_http2, ConnectError, pyo3::exceptions::PyException);
 create_exception!(async_http2, RequestError, pyo3::exceptions::PyException);
@@ -97,6 +125,7 @@ create_exception!(async_http2, BodyError, pyo3::exceptions::PyException);
 create_exception!(async_http2, BuilderError, pyo3::exceptions::PyException);
 create_exception!(async_http2, DecodeError, pyo3::exceptions::PyException);
 create_exception!(async_http2, RedirectError, pyo3::exceptions::PyException);
+create_exception!(async_http2, RetryError, pyo3::exceptions::PyException);
 
 #[pymodule]
 fn async_http2(py: Python, m: &PyModule) -> PyResult<()> {
@@ -114,6 +143,7 @@ fn async_http2(py: Python, m: &PyModule) -> PyResult<()> {
     m.add("BuilderError", py.get_type::<BuilderError>())?;
     m.add("DecodeError", py.get_type::<DecodeError>())?;
     m.add("RedirectError", py.get_type::<RedirectError>())?;
+    m.add("RetryError", py.get_type::<RetryError>())?;
 
     Ok(())
 }
